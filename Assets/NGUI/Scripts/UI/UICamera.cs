7d7acda81c0ef601114508a22d566799d6f0dc22
mercury233 <me@mercury233.me>
2019-04-10 19:19:49 +0800

    disable joystick

--- Assets/NGUI/Scripts/UI/UICamera.cs
+++ Assets/NGUI/Scripts/UI/UICamera.cs
@@ -236,7 +236,7 @@ public class UICamera : MonoBehaviour
 	/// Whether the joystick and controller events will be processed.
 	/// </summary>
 
-	public bool useController = true;
+	public bool useController = false;
 
 	[System.Obsolete("Use new OnDragStart / OnDragOver / OnDragOut / OnDragEnd events instead")]
 	public bool stickyPress { get { return true; } }
@@ -372,7 +372,7 @@ public class UICamera : MonoBehaviour
 		}
 	}
 
-	static bool mDisableController = false;
+	static bool mDisableController = true;
 	static Vector2 mLastPos = Vector2.zero;
 
 	/// <summary>
