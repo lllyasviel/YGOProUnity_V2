bf4541ae472d96a2ec67eb68113bf33d1818a1c8
mercury233 <me@mercury233.me>
2023-05-28 09:45:15 +0800

    version

--- Assets/SibylSystem/Config.cs
+++ Assets/SibylSystem/Config.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 public static class Config
 {
-    public static uint ClientVersion = 0x1354;
+    public static uint ClientVersion = 0x1360;
 
     class oneString
     {
